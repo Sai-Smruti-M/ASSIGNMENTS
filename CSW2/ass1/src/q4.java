869ee5fcbf6e06b5d4572705266a5d63bc896ca9
Sai-Smruti-M <163166823+Sai-Smruti-M@users.noreply.github.com>
2024-04-19 09:51:47 +0530

    Update q4.java

--- CSW2/ass1/src/q4.java
+++ CSW2/ass1/src/q4.java
@@ -32,6 +32,10 @@ public class q4
 
 }
 //toString() method returns an object's value as string 
+//It is a method of the object class which is the super class to all the class
+//we override this method to provide our own functionality
+//when we dont use to String and try to print an object directly it will give us the reference value of the object
+//but after the usage of toString method one can directly print the values of the object as string format
 //it is used in comparing purpose where we can compare an object to our desired result
 //if we don't use toString method to compare it can compare the objects reference value 
 //in place of its constraint values
