e2552219ab62758712991e3111f81e20c4b6dacd
Sai-Smruti-M <163166823+Sai-Smruti-M@users.noreply.github.com>
2024-04-22 22:29:20 +0530

    Update q8.java

--- CSW2/ass3/src/q8/q8.java
+++ CSW2/ass3/src/q8/q8.java
@@ -2,40 +2,43 @@ package q8;
 import java.util.HashMap;
 
 public class q8 {
-    public static void main(String[] args)
-    {
-        String str1 = "listen";
-        String str2 = "silent";
+    public static void main(String[] args) 
+	{
+		String s1="College";
+		String s2="gellcoe";
+		HashMap<Character,Integer> h1=new HashMap<>();
+		HashMap<Character,Integer> h2=new HashMap<>();
+		for(Character c:s1.toCharArray())
+		{
+			if(!h1.containsKey(c))
+			{
+				h1.put(Character.toLowerCase(c), 1);
+			}
+			else
+			{
+				Integer val=h1.get(Character.toLowerCase(c));
+				val+=1;
+				h1.put(Character.toLowerCase(c),val);
+			}
+		}
+		for(Character c:s2.toCharArray())
+		{
+			if(!h2.containsKey(c))
+			{
+				h2.put(Character.toLowerCase(c), 1);
+			}
+			else
+			{
+				Integer val=h2.get(Character.toLowerCase(c));
+				val+=1;
+				h2.put(Character.toLowerCase(c),val);
+			}
+		}
+		if(h1.equals(h2))
+		System.out.println("anagram");
+		else
+			System.out.println("not anagram");
 
-        if (areAnagrams(str1, str2)) {
-            System.out.println("Strings are anagrams.");
-        } else {
-            System.out.println("Strings are not anagrams.");
-        }
-    }
+	}
 
-    public static boolean areAnagrams(String str1, String str2) {
-        if (str1.length() != str2.length()) {
-            return false;
-        }
-
-        HashMap<Character, Integer> charCount1 = new HashMap<>();
-        HashMap<Character, Integer> charCount2 = new HashMap<>();
-
-        for (char c : str1.toCharArray()) {
-            charCount1.put(c, charCount1.getOrDefault(c, 0) + 1);
-        }
-
-        for (char c : str2.toCharArray()) {
-            charCount2.put(c, charCount2.getOrDefault(c, 0) + 1);
-        }
-
-        for (char c : charCount1.keySet()) {
-            if (!charCount2.containsKey(c) || !charCount2.get(c).equals(charCount1.get(c))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
 }
